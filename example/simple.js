24eae2a27e6f2a4e44d11b2c3003524a319c30fa
bradleymeck <bradley.meck@gmail.com>
2011-11-20 15:22:17 -0600

    [dist] 0.1.1, cli available now

--- example/simple.js
+++ example/simple.js
@@ -5,22 +5,30 @@ var client = nexmo.createClient({
   secret: '---'
 });
 
-//client.getBalance(function printBalance(error, value) {
-//  console.log('balance', value);
-//});
+client.getBalance(function printBalance(error, value) {
+  console.log('balance', value);
+});
 
-//client.getPricing('US', function printPricing(error, value) {
-//  console.log('pricing', value);
-//});
+client.getPricing('US', function printPricing(error, value) {
+  console.log('pricing', arguments);
+});
 
-//client.updateSettings('---', function printSettings(error, value) {
-//  console.log(value);
-//});
+client.updateSettings('---', function printSettings(error, value) {
+  console.log('settings', value);
+});
+
+client.getNumbers(function printNumbers(error, value) {
+  console.log('numbers', value);
+})
+
+client.searchNumbers({"country-code":'US',"pattern":"11"},function printNumbers(error, value) {
+  console.log(value);
+})
 
 client.send({
-  from: 'nodenexmo',
-  to: '15555555555',
+  from: 'MyCompany20',
+  to: '15124978584',
   text: 'test!'
 }, function printSend(error, value) {
   console.error(value.messages);
-})
+});
